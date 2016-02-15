87ec114dd259bd05840d8f4e9cb046e3532d656f
Ansel Adams <ansel@MacMini.local>
2016-02-14 23:04:03 -0700

    another commit

---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,15 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by Ansel Adams on 2/14/16.
+//  Copyright © 2016 Ansel Adams. All rights reserved.
+//
+
+import Foundation
+
+struct TaskModel {
+    var task: String
+    var subTask: String
+    var date: String
+}
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var taskArray: [TaskModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: "01/14/2014")
+        let task2 = TaskModel(task: "Eat Dinner", subTask: "Burgers", date: "01/14/2014")
+        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Leg day", date: "01/14/2014")]
+        
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let thisTask = taskArray[indexPath.row]
+        
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as! TaskCell
+        
+        cell.taskLabel.text = thisTask.task
+        cell.descriptionLabel.text = thisTask.subTask
+        cell.dateLabel.text = thisTask.date
+        
+        return cell
+    }
+    
+    // UITableViewDelegate
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
 
